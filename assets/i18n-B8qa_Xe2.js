00d7104c66908b2335f55cfae5def6da8343bd43
Eaglorns <eaglorn@yandex.ru>
2025-07-22 16:07:59 +1000

    v0.0.3

---
+++ assets/i18n-B8qa_Xe2.js
@@ -0,0 +1,1 @@
+import{d as a}from"./index-dYt88TZh.js";import{c as n}from"./vue-i18n.runtime-D2Sj-CDz.js";const s={components:{automatic:{automaticBuyers:{quantity:"Quantity"}}}},e={components:{automatic:{automaticBuyers:{quantity:"Количество"}}}},c={en:s,ru:e},m=a(({app:t})=>{const o=n({locale:"ru",messages:c});t.use(o)});export{m as default};
